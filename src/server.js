b2af96a9be62902b74b5fc43e1fe7ee79b9901f7
Jovi Simons <jovisimons009@gmail.com>
2022-01-10 14:54:00 +0100

    mixed code

--- src/server.js
+++ src/server.js
@@ -1,14 +1,31 @@
-const webSocketsServerPort = 8000;
-const webSocketsServer = require('websocket').server;
-const http = require('http');
+var express = require('express'); // Get the module
+const { Server } = require('ws');
+const PORT = process.env.PORT || 8000;
+const INDEX = '/index.html';
 
-const server = http.createServer();
-server.listen(webSocketsServerPort);
-console.log('listening on port ' + webSocketsServerPort);
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-const wsServer = new webSocketsServer({
-    httpServer: server
-});
+
+
+
+
+
+const wss = new Server({ server });
+
+
+wss.on('connection', (ws) => {
+    console.log('Client connected');
+    ws.on('close', () => console.log('Client disconnected'));
+  });
+
+
+  setInterval(() => {
+    wss.clients.forEach((client) => {
+      client.send(new Date().toTimeString());
+    });
+  }, 1000);
 
 const clients = {};
 
@@ -17,7 +34,7 @@ const getUniqueID = () => {
     return s4() + s4() + '-' + s4();
 };
 
-wsServer.on('request', function (request) {
+wss.on('request', function (request) {
     var userID = getUniqueID();
     console.log((new Date()) + 'Received a new connection from origin ' + request.origin);
 
