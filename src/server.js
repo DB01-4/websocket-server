ae59cd0b26ecc96dcb784bb12203344e0965d802
Jovi Simons <jovisimons009@gmail.com>
2022-01-10 14:36:32 +0100

    added working code

--- src/server.js
+++ src/server.js
@@ -1,28 +1,38 @@
-var express = require('express'); // Get the module
-const { Server } = require('ws');
-const PORT = process.env.PORT || 8000;
-const INDEX = '/index.html';
+const webSocketsServerPort = 8000;
+const webSocketsServer = require('websocket').server;
+const http = require('http');
 
-const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+const server = http.createServer();
+server.listen(webSocketsServerPort);
+console.log('listening on port ' + webSocketsServerPort);
 
+const wsServer = new webSocketsServer({
+    httpServer: server
+});
 
+const clients = {};
 
+const getUniqueID = () => {
+    const s4 = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+    return s4() + s4() + '-' + s4();
+};
 
+wsServer.on('request', function (request) {
+    var userID = getUniqueID();
+    console.log((new Date()) + 'Received a new connection from origin ' + request.origin);
 
+    const connection = request.accept(null, request.origin);
+    clients[userID] = connection;
+    console.log('connected: ' + userID + ' in ' + Object.getOwnPropertyNames(clients));
 
-const wss = new Server({ server });
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+            console.log('Received Message: ', message.utf8Data);
 
-
-wss.on('connection', (ws) => {
-    console.log('Client connected');
-    ws.on('close', () => console.log('Client disconnected'));
-  });
-
-
-  setInterval(() => {
-    wss.clients.forEach((client) => {
-      client.send(new Date().toTimeString());
-    });
-  }, 1000);
+            for(const key in clients) {
+                clients[key].sendUTF(message.utf8Data);
+                console.log('sent Message to: ', clients[key])
+            }
+        }
+    })
+})
